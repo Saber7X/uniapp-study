543f117eec735f4bfd16b7efd6d7909ed7824804
3050460876@qq.com <3050460876@qq.com>
2024-07-14 23:56:58 +0800

    配置了uni-ui和它的帮助插件

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
-//
+    //
 </script>
 
 <template>
-  <view class="index">index</view>
+    <view class="index">index</view>
+    <uni-badge text="1"></uni-badge>
+    <uni-badge text="2" type="success" @click="bindClick"></uni-badge>
+    <uni-badge text="3" type="primary" :inverted="true"></uni-badge>
+    <uni-card title="基础卡片" extra="额外信息" sub-title="副标题"
+        thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png">
+        <text>这是一个基础卡片示例，内容较少，此示例展示了一个没有任何属性不带阴影的卡片。</text>
+    </uni-card>
 </template>
 
 <style lang="scss">
-//
+    //
 </style>
--- src/stores/modules/member.ts
+++ src/stores/modules/member.ts
@@ -3,30 +3,32 @@ import { ref } from 'vue'
 
 // 定义 Store
 export const useMemberStore = defineStore(
-  'member',
-  () => {
-    // 会员信息
-    const profile = ref<any>()
+    'member', // 1. Store 的唯一标识名称
 
-    // 保存会员信息，登录时使用
-    const setProfile = (val: any) => {
-      profile.value = val
-    }
+    () => { // 2. Store 的定义函数
+        // 会员信息
+        const profile = ref<any>() // 3. 使用 ref 定义一个响应式变量 profile，初始值为 undefined，类型为 any
 
-    // 清理会员信息，退出时使用
-    const clearProfile = () => {
-      profile.value = undefined
-    }
+        // 保存会员信息，登录时使用，接受一个值
+        const setProfile = (val : any) => { // 4. 定义一个函数 setProfile，用于设置 profile 的值
+            profile.value = val // 5. 将传入的参数 val 赋值给 profile 的响应式值
+        }
 
-    // 记得 return
-    return {
-      profile,
-      setProfile,
-      clearProfile,
-    }
-  },
-  // TODO: 持久化
-  {
-    persist: true,
-  },
+        // 清理会员信息，退出时使用
+        const clearProfile = () => { // 6. 定义一个函数 clearProfile，用于清空 profile 的值
+            profile.value = undefined // 7. 将 profile 的响应式值设置为 undefined
+        }
+
+        // 记得 return
+        return { // 8. 返回 store 中定义的状态和方法
+            profile, // 9. 返回响应式变量 profile
+            setProfile, // 10. 返回设置 profile 的方法 setProfile
+            clearProfile, // 11. 返回清空 profile 的方法 clearProfile
+        }
+    },
+
+    // TODO: 持久化
+    {
+        persist: true, // 12. 配置持久化，将 store 的状态持久化到本地存储
+    },
 )
